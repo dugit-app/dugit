e5bad6ec61365566796175c17a84f123f024e34e
Brayden O'Neal <braydenoneal@protonmail.com>
2025-01-28 20:40:52 -0600

    Fix test workflow path

--- src/test/grade/add/add.test.ts
+++ src/test/grade/add/add.test.ts
@@ -11,10 +11,6 @@ test('removes the new grade', { timeout: 0 }, async () => {
     await expect(remove()).resolves.toBe(true)
 })
 
-test('fail intentionally to test workflow', () => {
-    expect(false).toBe(true)
-})
-
 async function add(): Promise<boolean> {
     const { assignment, classroom } = await getAssignment()
 
