37f7af3f5e73c9bd8fd5b0a5486cef72d8366aaf
Brayden O'Neal <braydenoneal@protonmail.com>
2025-02-04 10:57:44 -0600

    Fix spinner showing when update available message shown - close #27

--- src/utils/startup/startup.ts
+++ src/utils/startup/startup.ts
@@ -3,38 +3,42 @@ import { isLoggedIn, login } from '@/utils/auth/auth.js'
 import { version } from '@/utils/config/config.js'
 import chalk from 'chalk'
 import { exit } from 'node:process'
-import ora from 'ora'
+import ora, { Ora } from 'ora'
 import { simpleGit } from 'simple-git'
 
 export async function startup() {
     const spinner = ora().start()
-    await checkGitInstalled()
-    await checkUpdate()
-    await checkLoggedIn()
+    await checkGitInstalled(spinner)
+    await checkUpdate(spinner)
+    await checkLoggedIn(spinner)
     spinner.stop()
 }
 
-async function checkGitInstalled() {
+async function checkGitInstalled(spinner: Ora) {
     const gitInstalled = (await simpleGit().version()).installed
 
     if (!gitInstalled) {
+        spinner.stop()
         console.log(chalk.yellow('Git is not installed on this system. Please install Git in order to use Dugit'))
         exit()
     }
 }
 
-async function checkUpdate() {
+async function checkUpdate(spinner: Ora) {
     const latestVersion = (await getRepoFile('dugit-app', 'package.json', 'dugit')).version
     const installedVersion = version
 
     if (installedVersion != latestVersion) {
+        spinner.stop()
         console.log(`Dugit update available ${chalk.yellow(installedVersion)} -> ${chalk.green(latestVersion)}`)
         console.log(`Run ${chalk.underline('npm update -g dugit')} to update\n`)
+        spinner.start()
     }
 }
 
-async function checkLoggedIn() {
+async function checkLoggedIn(spinner: Ora) {
     if (!await isLoggedIn()) {
+        spinner.stop()
         console.log('You need to authenticate with GitHub to use Dugit\n')
         await login()
     }
