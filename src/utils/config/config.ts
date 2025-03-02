b8db2eb0e074e37c4bce4a992942f89384654253
Brayden O'Neal <braydenoneal@protonmail.com>
2025-03-02 10:06:08 -0600

    Bump version to 1.2.5

--- src/utils/config/config.ts
+++ src/utils/config/config.ts
@@ -6,7 +6,7 @@ export const clientID = 'Iv23lijtn2t2viXRUXCe'
 export const appID = 960869
 export const appInstallationLink = 'https://github.com/apps/dugit-app/installations/select_target'
 export const repoLink = 'https://github.com/dugit-app/dugit'
-export const version = '1.2.4'
+export const version = '1.2.5'
 
 export const configDirectoryPath = join(homedir(), '.config/', appName)
 export const configFilePath = join(configDirectoryPath, 'config.json')
